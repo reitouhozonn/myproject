627eca3b698a99d93dd6cb89da9c15245d8e1c50
teionhozonn <teionhozonn@gmail.com>
2021-06-23 23:16:30 +0900

    counter up

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -2,15 +2,24 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Counter extends Component
 {
     public $count = 0;
+    public $message;
 
     public function inc()
     {
         $this->count++;
+    }
+
+    public $users;
+
+    public function mount()
+    {
+        $this->users = User::all();
     }
 
     public function render()
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -4,4 +4,21 @@
     <h2>{{ $count }}</h2>
 
     <p><button wire:click="inc">+1</button></p>
+
+    <input type="text" wire:model.lazy="message">
+    <span>{{ $message }}</span>
+    <br>
+    @if (!$message)
+        <p style="color:red; font-weight: bold">入力してください</p>
+    @endif
+
+    <div>
+        <h2>users</h2>
+        <ul>
+            @foreach ($users as $user)
+                <li>{{ $user->name }}</li>
+            @endforeach
+        </ul>
+    </div>
+    
 </div>
